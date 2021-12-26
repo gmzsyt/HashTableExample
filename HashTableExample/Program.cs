1bc11d42e644452b45f4b679e2e465703a1a5f90
Gamze Seyit <gmzzsyyt@gmail.com>
2021-12-26 18:48:21 +0300

    Add project files.

---
+++ HashTableExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable Sozluk = new Hashtable();
+            Sozluk.Add("Araba", "Car");
+            Sozluk.Add("Bilgisayar", "Computer");
+            Sozluk["Kitap"] = " Book";
+            ICollection anahtarlar = Sozluk.Keys;
+
+             foreach (string anahtar in anahtarlar)
+               Console.WriteLine(anahtar + " " + Sozluk[anahtar]);
+
+           /* IDictionaryEnumerator ide = Sozluk.GetEnumerator();
+            while(ide.MoveNext())
+                Console.WriteLine(ide.Key + " "+ ide.Value);  */
+        }
+    }
+}
